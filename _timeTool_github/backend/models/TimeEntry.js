19af8a2d8d4c12fb3a0e4592b60768adff25fa42
ctacont <ctacont@freenet.de>
2025-06-21 22:16:09 +0200

    Delete _timeTool_github directory

--- _timeTool_github/backend/models/TimeEntry.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const timeEntrySchema = new mongoose.Schema({
-  username: String,
-  startTime: Date,
-  endTime: Date
-});
-
-module.exports = mongoose.model('TimeEntry', timeEntrySchema);
